86ca2167a92d3d06df082b1cdfdbc70de7955c5c
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-09 18:03:05 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,9 +39,11 @@ end
 end
 
 def for_iterator(number_of_times)
-
+  for counter in 1..number_of_times do
+   puts "Welcome to Flatiron School's Web Development Course!"
+  end
   
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+ # phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
